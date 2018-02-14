dfdc44d4e097c15fd208fb2ba6e9a82c5721a9d0
Maíra Tabata <mairatabata@outlook.com>
2018-02-14 18:35:48 -0200

    Criando função com elemento jsx

---
+++ src/Person/Person.js
@@ -0,0 +1,7 @@
+import React from 'react'; //importando de reack package
+
+const person = () => { //usando arrow function e colocando em uma constante
+    return <p>I'm a person!</p>
+};
+
+export person; //exportando para usar em outro lugar
